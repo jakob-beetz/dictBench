46ddd5a46745b323d9852f594276e605cf498120
Jakob Beetz <j.beetz@caad.arch.rwth-aachen.de>
2025-08-31 13:14:34 +0200

    adapt ALLOWED_HOSTS in settings.py  for testserver

--- propbench/propbench/settings.py
+++ propbench/propbench/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-propbench-dev-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dictmanager.semantic-collab.eu', 'localhost']
 
 # Application definition
 
